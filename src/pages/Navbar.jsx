10a50614675553f13a540a4fcd3d97216d5d0ffe
Sundari31 <sundarimuthaiah2004@gmail.com>
2025-08-19 13:07:34 +0530

    Update Project

---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import {Link} from "react-router-dom"
+import { WatchListContext } from '../context/WatchListContext'
+
+const Navbar = () => {
+
+const {watchlist} = useContext(WatchListContext)
+
+  return (
+   <nav className='bg-gray-900 p-4 text-white flex justify-between fixed w-full z-10'>
+    <Link className='text-xl font-bold' to="/">Movie App</Link>
+    <Link className='text-xl' to="/watchlist">Watchlist({watchlist.length})</Link>
+   </nav>
+  )
+}
+
+export default Navbar
